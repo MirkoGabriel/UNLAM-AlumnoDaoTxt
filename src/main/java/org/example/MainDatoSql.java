6b776c991bcd9886a18f60bf125003de5fe23ac1
mirkog-navent <mpinas@navent.com>
2023-06-03 13:35:17 -0300

    add crud into dao sql

--- src/main/java/org/example/MainDatoSql.java
+++ src/main/java/org/example/MainDatoSql.java
@@ -2,6 +2,7 @@ package org.example;
 
 import person.*;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,12 +10,21 @@ public class MainDatoSql {
     public static void main(String[] args) {
         try {
             Dao<Student, Integer> daoStudentSql= new StudentDaoSql("jdbc:mysql://127.0.0.1:3306/universidad_caba","root","root");
-            MyCalendar birthday = new MyCalendar(3, 4, 1976);
+            MyCalendar birthday = new MyCalendar(23, 1, 1998);
             MyCalendar admissionDate = new MyCalendar(11, 6, 2023);
 
-            Student student = new Student(admissionDate, 23, 7.1, 'F', 4512121, "Martina", "gerez", birthday);
+            Student student = new Student(admissionDate, 23, 7.1, 'M', 93873483, "Mirkito", "gerez", birthday);
 
-            daoStudentSql.create(student);
+            //daoStudentSql.create(student);
+           /* List<Student> students = daoStudentSql.findAll(true);
+            students.forEach(student1 -> {
+                System.out.println(student1.toString());
+            });*/
+         /*   Student studentRead = daoStudentSql.read(93873483);
+            System.out.println(studentRead.toString());*/
+
+          //  daoStudentSql.update(student);
+            daoStudentSql.delete(93873483);
         } catch (DaoException | MyCalendarException | PersonNameException | StudentException |
                  PersonDniException e) {
             Logger.getLogger(StudentDaoTxt.class.getName()).log(Level.SEVERE, null, e);
--- src/main/java/org/example/StudentDaoSql.java
+++ src/main/java/org/example/StudentDaoSql.java
@@ -2,20 +2,41 @@ package org.example;
 
 import person.*;
 
-import java.io.RandomAccessFile;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDaoSql extends Dao<Student, Integer> {
     private Connection conn;
-    private PreparedStatement ps;
+    private PreparedStatement psInsert;
+    private PreparedStatement psSelectAll;
+    private PreparedStatement psSelect;
+    private PreparedStatement psUpdate;
+    private PreparedStatement psDelete;
 
     public StudentDaoSql(String url, String user, String pass) throws DaoException {
-        //jdbc:mysql://127.0.0.1:3306/universidad_caba
         try {
             conn = DriverManager.getConnection(url, user, pass);
+
+            //CREATE
             String sqlInsert = "INSERT INTO students(dni, name, surname,gender,birthday,admission_date,approved_subject_quantity,average) VALUES(?,?,?,?,?,?,?,?)";
-            ps = conn.prepareStatement(sqlInsert);
+            psInsert = conn.prepareStatement(sqlInsert);
+
+            //SELECT ALL
+            String sqlSelectAll = "SELECT * FROM students";
+            psSelectAll = conn.prepareStatement(sqlSelectAll);
+
+            //SELECT
+            String sqlSelect = "SELECT * FROM students WHERE dni = ?";
+            psSelect = conn.prepareStatement(sqlSelect);
+
+            //UPDATE
+            String sqlUpdate = "UPDATE students SET name = ?, surname = ?, gender = ?, birthday = ?, admission_date = ?, approved_subject_quantity = ?, average = ? WHERE dni = ?";
+            psUpdate = conn.prepareStatement(sqlUpdate);
+
+            //DELETE
+            String sqlDelete = "DELETE FROM students WHERE dni = ?";
+            psDelete = conn.prepareStatement(sqlDelete);
         } catch (SQLException e) {
             throw new DaoException("Error to connect to database" + e.getLocalizedMessage());
         }
@@ -24,38 +45,96 @@ public class StudentDaoSql extends Dao<Student, Integer> {
     @Override
     public void create(Student student) throws DaoException {
         try {
-            ps.setInt(1, student.getDni());
-            ps.setString(2, student.getName());
-            ps.setString(3, student.getSurname());
-            ps.setString(4, String.valueOf(student.getGender()));
-            ps.setDate(5, student.getBirthday().toSqlDate());
-            ps.setDate(6, student.getAdmissionDate().toSqlDate());
-            ps.setInt(7,student.getApprovedSubjectQuantity());
-            ps.setDouble(8,student.getAverage());
-            ps.execute();
+            psInsert.setInt(1, student.getDni());
+            psInsert.setString(2, student.getName());
+            psInsert.setString(3, student.getSurname());
+            psInsert.setString(4, String.valueOf(student.getGender()));
+            psInsert.setDate(5, student.getBirthday().toSqlDate());
+            psInsert.setDate(6, student.getAdmissionDate().toSqlDate());
+            psInsert.setInt(7, student.getApprovedSubjectQuantity());
+            psInsert.setDouble(8, student.getAverage());
+            psInsert.execute();
+            close();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException(e.getLocalizedMessage());
         }
     }
 
     @Override
-    public Student read(Integer id) throws DaoException {
-        return null;
+    public Student read(Integer dni) throws DaoException {
+        try {
+            Student student = new Student();
+            psSelect.setInt(1, dni);
+            ResultSet rs = psSelect.executeQuery();
+
+            if (rs.next()) {
+                student.setDni(rs.getInt("dni"));
+                student.setName(rs.getString("name"));
+                student.setSurname(rs.getString("surname"));
+                student.setBirthday(new MyCalendar(rs.getDate("birthday")));
+                student.setAdmissionDate(new MyCalendar(rs.getDate("admission_date")));
+                student.setGender(rs.getString("gender").charAt(0));
+                student.setApprovedSubjectQuantity(rs.getInt("approved_subject_quantity"));
+                student.setAverage(rs.getDouble("average"));
+            }
+            return student;
+        } catch (SQLException | PersonDniException | PersonNameException | StudentException e) {
+            throw new DaoException(e.getLocalizedMessage());
+        }
     }
 
     @Override
-    public void update(Student entity) throws DaoException {
-
+    public void update(Student student) throws DaoException {
+        try {
+            psUpdate.setString(1, student.getName());
+            psUpdate.setString(2, student.getSurname());
+            psUpdate.setString(3, String.valueOf(student.getGender()));
+            psUpdate.setDate(4, student.getBirthday().toSqlDate());
+            psUpdate.setDate(5, student.getAdmissionDate().toSqlDate());
+            psUpdate.setInt(6, student.getApprovedSubjectQuantity());
+            psUpdate.setDouble(7, student.getAverage());
+            psUpdate.setInt(8, student.getDni());
+            psUpdate.execute();
+            close();
+        } catch (SQLException e) {
+            throw new DaoException(e.getLocalizedMessage());
+        }
     }
 
     @Override
-    public void delete(Integer id) throws DaoException {
-
+    public void delete(Integer dni) throws DaoException {
+        try {
+            psDelete.setInt(1, dni);
+            psDelete.executeUpdate();
+            close();
+        } catch (SQLException e) {
+            throw new DaoException(e.getLocalizedMessage());
+        }
     }
 
     @Override
     public List<Student> findAll(boolean onlyActive) throws DaoException {
-        return null;
+        try {
+            List<Student> students = new ArrayList<>();
+            Student student = new Student();
+            ResultSet rs = psSelectAll.executeQuery();
+
+            while (rs.next()) {
+                student = new Student();
+                student.setDni(rs.getInt("dni"));
+                student.setName(rs.getString("name"));
+                student.setSurname(rs.getString("surname"));
+                student.setBirthday(new MyCalendar(rs.getDate("birthday")));
+                student.setAdmissionDate(new MyCalendar(rs.getDate("admission_date")));
+                student.setGender(rs.getString("gender").charAt(0));
+                student.setApprovedSubjectQuantity(rs.getInt("approved_subject_quantity"));
+                student.setAverage(rs.getDouble("average"));
+                students.add(student);
+            }
+            return students;
+        } catch (SQLException | PersonDniException | PersonNameException | StudentException e) {
+            throw new DaoException(e.getLocalizedMessage());
+        }
     }
 
     @Override
